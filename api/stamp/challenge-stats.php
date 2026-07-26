16c12ae9b0eb3ea6305630085839c6305399ef89
Kongtiao <kongtiao92@gmail.com>
2026-07-27 00:14:00 +0800

    Improve performance

--- api/stamp/challenge-stats.php
+++ api/stamp/challenge-stats.php
@@ -6,54 +6,56 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
   die_json(405, 'Invalid request method');
 }
 
-$stamp_column = [];
-$select = [];
+$stamp_ids = range(0, 9);
+$stamp_data = [];
+$challenge_totals = [];
+$challenge_ids = [];
 
-foreach (range(0, 9) as $stamp_id) {
-  $stamp_column[] = "stamp$stamp_id";
-  $select[] = "COUNT(*) FILTER (WHERE ss.stamp_id = $stamp_id) AS stamp$stamp_id";
-}
-
-$select_str = implode(', ', $select);
-$stamp_column_str = implode(', ', $stamp_column);
-
-$query = "SELECT 
-    c.*,
-    $stamp_column_str,
-    total
-  FROM (
-    SELECT 
-      c.id AS challenge_id,
-      $select_str,
-      COUNT(*) AS total
-    FROM stamp_submission ss
-    JOIN submission s ON s.id = ss.submission_id
-    JOIN challenge c ON c.id = s.challenge_id
-    GROUP BY c.id
-  ) AS t
-  JOIN view_challenges c ON c.challenge_id = t.challenge_id
-  ORDER BY total DESC";
-
+$query = "SELECT ss.stamp_id, s.challenge_id
+  FROM stamp_submission ss
+  JOIN submission s ON s.id = ss.submission_id";
 $result = pg_query_params_or_die($DB, $query);
+
+while ($row = pg_fetch_assoc($result)) {
+  $challenge_id = intval($row['challenge_id']);
+  $stamp_id = intval($row['stamp_id']);
+
+  if (!isset($stamp_data[$challenge_id])) {
+    $stamp_data[$challenge_id] = array_fill(0, count($stamp_ids), 0);
+    $challenge_totals[$challenge_id] = 0;
+    $challenge_ids[$challenge_id] = true;
+  }
+
+  $challenge_totals[$challenge_id]++;
+
+  if (in_array($stamp_id, $stamp_ids, true)) {
+    $stamp_data[$challenge_id][$stamp_id]++;
+  }
+}
 
 $response = [];
 
-while ($row = pg_fetch_assoc($result)) {
-  $row_data = [];
+if (count($challenge_ids) > 0) {
+  $challenge_id_list = "{" . implode(',', array_keys($challenge_ids)) . "}";
+  $query = "SELECT * FROM view_challenges WHERE challenge_id = ANY ($1)";
+  $result = pg_query_params_or_die($DB, $query, [$challenge_id_list]);
 
-  $row_data['challenge'] = new Challenge();
-  $row_data['challenge']->apply_db_data($row, 'challenge_');
-  $row_data['challenge']->expand_foreign_keys($row);
+  while ($row = pg_fetch_assoc($result)) {
+    $challenge_id = intval($row['challenge_id']);
 
-  $row_data['stamps'] = [];
+    $row_data = [];
+    $row_data['challenge'] = new Challenge();
+    $row_data['challenge']->apply_db_data($row, 'challenge_');
+    $row_data['challenge']->expand_foreign_keys($row, 5);
+    $row_data['stamps'] = $stamp_data[$challenge_id];
+    $row_data['total'] = $challenge_totals[$challenge_id];
 
-  foreach ($stamp_column as $column_name) {
-    $row_data['stamps'][] = intval($row[$column_name]);
+    $response[] = $row_data;
   }
-
-  $row_data['total'] = intval(($row['total']));
-
-  $response[] = $row_data;
 }
+
+usort($response, function ($left, $right) {
+  return $right['total'] <=> $left['total'];
+});
 
 api_write($response);
